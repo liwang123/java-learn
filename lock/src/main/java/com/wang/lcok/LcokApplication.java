47e8a0d7e3083abea3961e041433a5efd638fc50
liwang <wang.li@thingtrust.com>
2019-07-25 23:41:16 +0800

    ReentrantLock condition线程顺序输出

---
+++ lock/src/main/java/com/wang/lcok/LcokApplication.java
@@ -0,0 +1,13 @@
+package com.wang.lcok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LcokApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LcokApplication.class, args);
+    }
+
+}
